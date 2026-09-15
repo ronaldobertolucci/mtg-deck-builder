f7eb751468ca5d4bcfaebac9e72bde8c153ba67d
Ronaldo Bertolucci Jr <ronaldobertoluccijr@gmail.com>
2026-09-15 02:21:18 -0300

    fix: jackson CardDetailsResponse properties

--- src/main/java/io/github/ronaldobertolucci/mtgdeckbuilder/dto/card/CardDetailsResponse.java
+++ src/main/java/io/github/ronaldobertolucci/mtgdeckbuilder/dto/card/CardDetailsResponse.java
@@ -1,16 +1,24 @@
 package io.github.ronaldobertolucci.mtgdeckbuilder.dto.card;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 import java.util.List;
 import java.util.UUID;
 
+@JsonIgnoreProperties(ignoreUnknown = true)
 public record CardDetailsResponse(
+        @JsonProperty("oracle_id")
         UUID oracleId,
         String name,
+        @JsonProperty("type_line")
         String typeLine,
+        @JsonProperty("oracle_text")
         String oracleText,
+        @JsonProperty("color_identity")
         List<String> colorIdentity
 ) {
     public CardDetailsResponse {
-        colorIdentity = List.copyOf(colorIdentity);
+        colorIdentity = colorIdentity != null ? List.copyOf(colorIdentity) : List.of();
     }
 }
